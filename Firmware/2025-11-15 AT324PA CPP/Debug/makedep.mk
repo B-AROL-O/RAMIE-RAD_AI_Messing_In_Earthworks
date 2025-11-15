b12ca56e054e1565144172cf15038b58445db179
FoM <30684972+OrsoEric@users.noreply.github.com>
2025-11-15 16:46:17 +0100

    I rebuilt the AVR project using the C++ compiler and it compiles and load now Much bigger executable from 3 to 9KB so it's doing more Linked the parser Seems to be doing something But the serial decoding doesn't work and it's not easy to debug with avrisp no JTAG So I need to look at it tomorrow and figure out why it's not decoding the callback functions https://github.com/B-AROL-O/RAMIE-RAD_AI_Messing_In_Earthworks/issues/5

---
+++ Firmware/2025-11-15 AT324PA CPP/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+at_lcd.cpp
+
+at_string.cpp
+
+At_trig8.cpp
+
+initialisation.cpp
+
+interrupt.cpp
+
+main.cpp
+
+parser_handlers.cpp
+
+uniparser.cpp
+
