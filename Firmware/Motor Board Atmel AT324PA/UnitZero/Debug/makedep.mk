fb90388b9c74df02a5658591a0189bfb1a5f49cc
FoM <30684972+OrsoEric@users.noreply.github.com>
2025-11-15 16:29:25 +0100

    doesn't compile. something with C++? -.-

--- Firmware/Motor Board Atmel AT324PA/UnitZero/Debug/makedep.mk
+++ Firmware/Motor Board Atmel AT324PA/UnitZero/Debug/makedep.mk
@@ -2,15 +2,3 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-at_lcd.c
-
-at_string.c
-
-At_trig8.c
-
-initialisation.c
-
-interrupt.c
-
-main.c
-
